6e3cf5ebce2c75a0b3003efdd86c3cb6c10eb808
Zahed <zahedalam@workspaceit.com>
2017-05-17 19:07:13 +0600

    create pdf using pdfkit

---
+++ pdfkit/render.py
@@ -0,0 +1,27 @@
+import pdfkit
+import json
+from jinja2 import Environment, FileSystemLoader
+
+def hello():
+    with open('mydata.json') as data_file:
+        data = json.load(data_file)
+        env = Environment(loader=FileSystemLoader('.'))
+        template = env.get_template("mydata.html")
+        template_vars = {"data" : data}
+        # Render our file and create the PDF using our css style file
+        html_out = template.render(template_vars)
+        filename = 'render.html'
+        f = open(filename, 'w+')
+        f.write(html_out)
+        f.close()
+        options = {
+            'page-size': 'Letter',
+            'margin-top': '0.75in',
+            'margin-right': '0.75in',
+            'margin-bottom': '0.75in',
+            'margin-left': '0.75in'
+        }
+        pdfkit.from_file('render.html', 'render.pdf', options=options)
+
+if __name__ == "__main__":
+    hello()

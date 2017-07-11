618640037a208d2dc80d569e1e53b6de62c3a23a
Peter Styk <peter@styk.tv>
2017-07-11 16:29:30 +0100

    added email query to

--- xlswriter/run_from_disk.sh
+++ xlswriter/run_from_disk.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-PATH_DATA="~/tmp/allmail"
-python main.py --perspective DOMAIN --transport FILE --file_store_path $PATH_DATA
+source ~/builds/venv_containers/bin/activate
+DATA="/Users/polfilm/tmp/offlineimap"
+
+#python main.py --perspective DOMAIN --sections 'EmailAddress,Urls,Schemes,SubDomains' --transport FILE --file_store_path $DATA --verbose
+
+#python main.py --perspective DOMAIN --transport FILE --file_store_path $DATA --verbose
+#python main.py --perspective DOMAIN --sections 'EmailAddress,Urls,Schemes,SubDomains' --transport FILE --file_store_path $DATA --verbose
+python main.py --perspective EMAILADDRESS --transport FILE --file_store_path $DATA --verbose
